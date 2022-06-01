f5ad7cdc7f66a17c748b0625c0d4fbe4f51bc7c4
PritamMakWana <pritammakaWana17561@gmail.com>
2022-06-01 20:04:42 +0530

    spread opeartor}
    s
    s
    '}
    ~

---
+++ JS topics React Native/mechanism/js4.js
@@ -0,0 +1,34 @@
+
+function sum(name, ...args) {
+    console.log(arguments);
+
+    document.write(`Hello ${name} : <br>`);
+
+    let sum = 0;
+    for (let i in args) {
+        document.write(args[i] + `<br>`);
+        sum += args[i];
+    }
+
+    document.write(`total = ` + sum);
+
+}
+
+let arr = [10, 20, 30, 40];
+
+sum("admin", ...arr);
+//...arr {is a spread opeartor}
+
+let obj1 = {
+    name: "user"
+};
+let obj2 = {
+    age: 25,
+    work: "teacher"
+};
+
+let obj = { ...obj1, ...obj2 };
+
+console.log(obj);
+
+
